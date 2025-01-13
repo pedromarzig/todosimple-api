6d2aba448ca490c99660ce4bfb4f10c0ec1008ba
pedro marzig <pedromarzigcastelani@gmail.com>
2025-01-13 09:18:24 -0300

    Create User and Task repository classes

---
+++ src/main/java/com/lucasangelo/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,23 @@
+package com.lucasangelo.todosimple.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.lucasangelo.todosimple.models.Task;
+
+
+import java.util.List;
+
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
+    
+   List<Task> findByUser_Id(Long id);
+
+   //@Query(value = "SELECT t FROM task t WHERE t.user.id = :id")
+   //List<Task> findByUser_Id(@Param("id") Long id);
+   
+  // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+   //List<Task> findByUser_Id(@Param("id") Long id);
+
+}
