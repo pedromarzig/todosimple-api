14c9e03d67803ffee9acaa3de408d8dcd20d88fe
pedro marzig <pedromarzigcastelani@gmail.com>
2025-01-16 09:20:46 -0300

    Create TaskService

--- src/main/java/com/lucasangelo/todosimple/services/UserService.java
+++ src/main/java/com/lucasangelo/todosimple/services/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasangelo.todosimple.models.User;
-import com.lucasangelo.todosimple.repositories.TaskRepository;
 import com.lucasangelo.todosimple.repositories.UserRepository;
 
 @Service
@@ -17,8 +16,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
+    
 
 
     public User findById(Long id){
@@ -32,7 +30,6 @@ public class UserService {
     public User createUser(User obj){
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
